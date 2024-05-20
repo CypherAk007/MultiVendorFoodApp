450084bed2c52152764c3ec49f0b5b61e7bb72e3
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-05-20 18:49:57 +0530

    Initial Commit

---
+++ frontend/README.md
@@ -0,0 +1,7 @@
+## Create React app
+- npx create-react-app frontend
+
+## LUX Theme from bootswatch.com
+- Download bootstrap.min.css
+- https://bootswatch.com/
+
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <div>Myapp</div>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
